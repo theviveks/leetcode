aa00267531cb0ca0e200614d169f554c86434eaa
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-05-17 16:22:27 +0530

    Time: 14 ms (61.90%) | Memory: 48.3 MB (65.99%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,13 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i : nums)
-            map.merge(i, 1, Integer::sum);// For Getting Frequency
-        List<Integer> list = new ArrayList<>(map.keySet());
-        list.sort((a, b) -> map.get(b) - map.get(a)); // Sort by Frequency in descending order
-        int res[] = new int[k];
-        for (int i = 0; i < k; ++i)
-            res[i] = list.get(i);
-        return res;
+        // Find the frequency of each number
+        Map<Integer, Integer> numFrequencyMap = new HashMap<>();
+        for (int n : nums)
+            numFrequencyMap.put(n, numFrequencyMap.getOrDefault(n, 0) + 1);
+
+        PriorityQueue<Map.Entry<Integer, Integer>> topKElements = new PriorityQueue<>(
+                (e1, e2) -> e1.getValue() - e2.getValue());
+
+        // Go through all numbers of the numFrequencyMap and push them into
+        // topKElements, which will have
+        // the top k frequent numbers. If the heap size is more than k, we remove the
+        // smallest (top) number.
+        for (Map.Entry<Integer, Integer> entry : numFrequencyMap.entrySet()) {
+            topKElements.add(entry);
+            if (topKElements.size() > k) {
+                topKElements.poll();
+            }
+        }
+
+        // Create a list of top k numbers
+        int[] topNumbers = new int[k];
+
+        int i = 0;
+        while (!topKElements.isEmpty()) {
+            topNumbers[i] = topKElements.poll().getKey();
+            i++;
+        }
+
+        return topNumbers;
     }
 }
