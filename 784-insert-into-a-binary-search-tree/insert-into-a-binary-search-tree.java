9fb0c5ab558912f5527fa940280ba88c503095d6
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-05-18 14:47:05 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (97.86%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -1,42 +1,33 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root==null) return new TreeNode(val);
-        TreeNode cur= root;
-        while(true){
-            if(cur.val>=val){
-                  if(cur.left!=null){
-                    cur=cur.left;
-                  }
-                  else{
-                    cur.left=new TreeNode(val);
-                    break;
-                  }
-            }
-            else{
-                if(cur.right!=null){
-                    cur=cur.right;
-                  }
-                  else{
-                    cur.right=new TreeNode(val);
-                    break;
-                  }
-            }
+        if (root == null) {
+            root = new TreeNode(val);
+            return root;
         }
+
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            return root;
+        }
+
         return root;
     }
 }
