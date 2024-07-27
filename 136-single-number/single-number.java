3519c7913cc9fa07fdd2e309d74224c1d8a3229f
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-07-27 10:58:45 +0530

    Time: 12 ms (21.68%) | Memory: 44.8 MB (91.51%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -5,9 +5,9 @@ class Solution {
         for (int i = 0; i < n; i++) {
             mapp.put(nums[i], mapp.getOrDefault(nums[i], 0) + 1);
         }
-        for (int i = 0; i < n; i++) {
-            if (mapp.get(nums[i]) == 1) {
-                return nums[i];
+        for(int num:nums){
+            if(mapp.get(num)==1){
+                return num;
             }
         }
         return -1;
