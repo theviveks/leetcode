8a66b77b0f77ea460c11583e8ffcf1c913092940
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-08-04 10:52:03 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (52.28%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -7,6 +7,7 @@ class Solution {
             i++;
             j--;
         }
+
     }
 
     public void swap(int[] nums, int i, int j) {
@@ -27,14 +28,15 @@ class Solution {
         if (index == -1) {
             reverse(nums, 0, n - 1);
             return;
-        }
-        for (int i = n - 1; i >= index; i--) {
-            if (nums[i] > nums[index]) {
-                swap(nums, i, index);
-                break;
+        } else {
+            for (int i = n - 1; i >= index; i--) {
+                if (nums[i] > nums[index]) {
+                    swap(nums, i, index);
+                    break;
+                }
             }
+            reverse(nums, index + 1, n - 1);
         }
-        reverse(nums, index + 1, n - 1);
 
     }
 }
