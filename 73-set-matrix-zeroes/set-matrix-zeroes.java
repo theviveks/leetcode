ac0b15da896aa56cd66b484dcffeacc7d2309785
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-08-06 20:27:34 +0530

    Time: 1 ms (80.49%) | Memory: 45.7 MB (25.78%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,22 +1,39 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int[] col = new int[m];
-        int[] row = new int[n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    col[j] = 1;
-                    row[i] = 1;
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int col0=1;
+        //col -> matrix[..][j]
+        //row -> matrix[i][..]
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+              if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        col0=0;
+                    }
+                }
+
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (row[i] == 1 || col[j] == 1) {
-                    matrix[i][j] = 0;
-                }
+        if(matrix[0][0]==0){
+            for(int i=0;i<m;i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(col0==0){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
             }
         }
     }
