6dc78ed8ebb0b3d907300fa904e6c32917ebaf05
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-08-14 07:49:15 +0530

    Time: 0 ms (100.00%) | Memory: 46 MB (29.58%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int firstOccurance(int[] nums, int n, int x) {
+    public int firstOccurrence(int[] nums, int n, int x) {
         int low = 0;
         int high = n - 1;
         int first = -1;
@@ -17,14 +17,14 @@ class Solution {
         return first;
     }
 
-    public int secondOccurance(int[] nums, int n, int x) {
+    public int lastOccurrence(int[] nums, int n, int x) {
         int low = 0;
         int high = n - 1;
-        int second = -1;
+        int last = -1;
         while (low <= high) {
             int mid = (low + high) / 2;
             if (nums[mid] == x) {
-                second = mid;
+                last = mid;
                 low = mid + 1;
             } else if (nums[mid] < x) {
                 low = mid + 1;
@@ -32,16 +32,13 @@ class Solution {
                 high = mid - 1;
             }
         }
-        return second;
+        return last;
     }
 
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
-        int first = firstOccurance(nums, n, target);
-        if (first == -1) {
-            return new int[] { -1, -1 };
-        }
-        int second = secondOccurance(nums, n, target);
-        return new int[] { first, second };
+        int first = firstOccurrence(nums, n, target);
+        int last = lastOccurrence(nums, n, target);
+        return new int[] { first, last };
     }
 }
