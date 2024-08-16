52d81d69389ea6521c3dc14c7cdffc25790fbe99
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-08-16 15:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (88.39%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -3,22 +3,7 @@ class Solution {
         if (goal.length() != s.length()) {
             return false;
         }
-        String rotated = s + s;
-        int j = 0;
-        int count = 0;
-        for (int i = 0; i < rotated.length(); i++) {
-            if (rotated.charAt(i) == goal.charAt(j)) {
-                count++;
-                j++;
-                if (count == goal.length()) {
-                    return true;
-                }
-            } else {
-                i -= count;
-                count = 0;
-                j = 0;
-            }
-        }
-        return false;
+        String c=s+s;
+        return c.contains(goal);
     }
 }
