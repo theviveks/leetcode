75670e1170c12012cbb557587618ea5629e243de
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-08-19 22:10:50 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (6.99%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if ((i == 0 || nums[i] > nums[i - 1]) && (i == n - 1 || nums[i + 1] < nums[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
