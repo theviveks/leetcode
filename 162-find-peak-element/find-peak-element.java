7eae4abfbb9ba56035b7126b11ae075a352379e7
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-08-19 22:36:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (19.48%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -14,7 +14,7 @@ class Solution {
         }
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+            if(nums[mid]>=nums[mid-1] && nums[mid]>=nums[mid+1]){
                 return mid;
             }
             else if(nums[mid]>nums[mid-1]){
