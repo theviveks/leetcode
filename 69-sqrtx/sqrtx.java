af14d1130a174a6093ac0be0d82636580f4bbe48
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-08-20 21:25:22 +0530

    Time: 22 ms (7.01%) | Memory: 40.8 MB (50.35%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mySqrt(int x) {
+        int ans=1;
+        if(x==0) return 0;
+        for(int i=1;i<x;i++){
+            if((long)i*i<=x){
+                ans=i;
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
+    }
+}
