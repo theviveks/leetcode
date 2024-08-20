a7889e0c0f52469ea488b3164fbb174fea5eed33
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-08-20 21:28:39 +0530

    Time: 22 ms (7.01%) | Memory: 40.8 MB (36.08%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -3,7 +3,7 @@ class Solution {
         int ans=1;
         if(x==0) return 0;
         for(int i=1;i<x;i++){
-            if((long)i*i<=x){
+            if((long) i*i<=x){
                 ans=i;
             }
             else{
