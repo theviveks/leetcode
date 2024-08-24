751d93ee1c915f83715b929c53607d502d79b0d8
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-08-24 18:45:59 +0530

    Time: 6 ms (57.93%) | Memory: 46.3 MB (6.96%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -20,9 +20,10 @@ class Solution {
         int m = matrix[0].length;
         for (int i = 0; i < n; i++) {
             if (matrix[i][0] <= target && matrix[i][m - 1] >= target) {
-                if (binarySearch(matrix[i], m, target)) {
-                    return true;
-                }
+                    if(binarySearch(matrix[i], m, target)){
+                        return true;
+                    }
+                
             }
         }
         return false;
