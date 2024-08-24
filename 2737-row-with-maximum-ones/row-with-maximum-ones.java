6b186ab94dda1d57d5bbe33ee323ada68046e8d9
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-08-24 11:24:50 +0530

    Time: 8 ms (7.88%) | Memory: 46.9 MB (8.69%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.java
@@ -1,20 +1,33 @@
 class Solution {
-    public int[] rowAndMaximumOnes(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int index = 0;
-        int countMax = 0;
-        for (int i = 0; i < n; i++) {
-            int countSum = 0;
-            for (int j = 0; j < m; j++) {
-                countSum += mat[i][j];
+    public int lowerBound(int[] mat,int m,int x){
+        Arrays.sort(mat);
+        int low=0;
+        int high=m-1;
+        int ans=m;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(mat[mid]>=x){
+                ans=mid;
+                high=mid-1;
             }
-            if (countSum > countMax) {
-                countMax = countSum;
-                index = i;
-
+            else{
+                low=mid+1;
             }
         }
-        return new int[] { index, countMax };
+        return ans;
+    }
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        int index=-1;
+        int countMax=-1;
+        for(int i=0;i<n;i++){
+           int count=m-(lowerBound(mat[i],m,1));
+           if(count>countMax){
+            countMax=count;
+            index=i;
+           }
+        }
+        return new int[]{index,countMax};        
     }
 }
