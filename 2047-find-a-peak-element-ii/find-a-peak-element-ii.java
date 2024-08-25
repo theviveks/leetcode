94f7c7cb1a00a95dfe5f73c44587002d8411ff4d
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-08-25 11:59:55 +0530

    Time: 0 ms (100.00%) | Memory: 90 MB (24.23%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,18 +1,39 @@
 class Solution {
+    public int findMaxIndex(int[][] mat, int n, int col) {
+        int index = -1;
+        int maxIndex = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (mat[i][col] > maxIndex) {
+                maxIndex = mat[i][col];
+                index = i;
+            }
+        }
+        return index;
+    }
+
     public int[] findPeakGrid(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        int maxElement = Integer.MIN_VALUE;
-        int[] peakPosition = new int[2];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] > maxElement) {
-                    maxElement = mat[i][j];
-                    peakPosition[0] = i;
-                    peakPosition[1] = j;
-                }
+        int low = 0;
+        int high = m - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int maxIndexValue = findMaxIndex(mat, n, mid);
+
+            int midValue = mat[maxIndexValue][mid];
+            int left = mid - 1 >= 0 ? mat[maxIndexValue][mid - 1] : Integer.MIN_VALUE;
+            int right = mid + 1 < m ? mat[maxIndexValue][mid + 1] : Integer.MIN_VALUE;
+
+            if (midValue > left && midValue > right) {
+                return new int[] { maxIndexValue, mid };
+            } else if (midValue < left) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return peakPosition;
+
+        return new int[] { -1, -1 };
     }
 }
