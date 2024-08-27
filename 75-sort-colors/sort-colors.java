15a4077df9083c2e7319535beb5cb62bb908e997
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-08-27 18:59:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (59.49%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,41 +1,37 @@
 class Solution {
-    public void merge(int[] nums, int low, int mid, int high) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        int left = low;
-        int right = mid + 1;
-        while (left <= mid && right <= high) {
-            if (nums[left] < nums[right]) {
-                ans.add(nums[left]);
-                left++;
-            } else {
-                ans.add(nums[right]);
-                right++;
+    public int partition(int[] nums, int low, int high) {
+        int pivot = nums[low];
+        int i = low;
+        int j = high;
+        while (i < j) {
+            while (nums[i] <= pivot && i <= high - 1) {
+                i++;
+            }
+            while (nums[j] > pivot && j >= low + 1) {
+                j--;
+            }
+            if (i < j) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
             }
         }
-        while (left <= mid) {
-            ans.add(nums[left]);
-            left++;
-        }
-        while (right <= high) {
-            ans.add(nums[right]);
-            right++;
-        }
-        for (int i = low; i <= high; i++) {
-            nums[i] = ans.get(i - low);
-        }
+        int temp = nums[low];
+        nums[low] = nums[j];
+        nums[j] = temp;
+        return j;
     }
 
-    public void mergeSort(int[] nums, int low, int high) {
-        if (low == high)
-            return;
-        int mid = (low + high) / 2;
-        mergeSort(nums, low, mid);
-        mergeSort(nums, mid + 1, high);
-        merge(nums, low, mid, high);
+    public void quickSort(int[] nums, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(nums, low, high);
+            quickSort(nums, low, pivotIndex - 1);
+            quickSort(nums, pivotIndex + 1, high);
+        }
     }
 
     public void sortColors(int[] nums) {
         int n = nums.length;
-        mergeSort(nums, 0, n - 1);
+        quickSort(nums, 0, n - 1);
     }
 }
