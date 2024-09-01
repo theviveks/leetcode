93af3aa2989ff9d265ee9ba36a957cdd853110da
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-09-01 15:33:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (42.27%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+
+        long m = n;
+        if (m < 0) {
+            x = 1 / x;
+            m = -m;
+        }
+        double ans = 1;
+        while (m > 0) {
+            if (m % 2 == 1) {
+                ans = ans * x;
+                m = m - 1;
+            } else {
+                x = x * x;
+                m /= 2;
+            }
+        }
+        return ans;
+
     }
 }
