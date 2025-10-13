1abb96657a5e7b0bd94bf5a40a5f94d770984992
VIVEK S <138084941+theviveks@users.noreply.github.com>
2025-10-13 19:52:10 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (37.34%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -14,21 +14,22 @@
  * }
  */
 class Solution {
-    public void dfs(TreeNode node,List<List<Integer>>ans, int level){
-        if(node==null){
+    public void dfs(TreeNode node, List<List<Integer>> ans, int level) {
+        if (node == null) {
             return;
         }
         if (ans.size() == level) {
             ans.add(new ArrayList<>());
         }
-        
+
         ans.get(level).add(node.val);
-        dfs(node.left,ans,level+1);
-        dfs(node.right,ans,level+1);
+        dfs(node.left, ans, level + 1);
+        dfs(node.right, ans, level + 1);
     }
+
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
-        dfs(root,ans,0);
+        dfs(root, ans, 0);
         return ans;
     }
 }
