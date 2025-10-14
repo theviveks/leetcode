a2daeeb728ef16ef6992656f519bdb72b81d0d87
VIVEK S <138084941+theviveks@users.noreply.github.com>
2025-10-14 12:29:51 +0530

    Time: 1 ms (12.47%) | Memory: 42.3 MB (6.86%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -14,23 +14,31 @@
  * }
  */
 class Solution {
-    private boolean helper(TreeNode left, TreeNode right) {
-        if (left == null && right == null) {
-            return true;
-        }
-        if (left == null || right == null) {
-            return false;
-        }
-        if (left.val != right.val) {
-            return false;
-        }
-        return helper(left.left, right.right) && helper(left.right, right.left);
-    }
-
     public boolean isSymmetric(TreeNode root) {
         if (root == null) {
             return false;
         }
-        return helper(root.left, root.right);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root.left);
+        q.offer(root.right);
+        while (!q.isEmpty()) {
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+            if (left == null && right == null) {
+                continue;
+            }
+            if (left == null || right == null) {
+                return false;
+            }
+            if (left.val != right.val) {
+                return false;
+            }
+            q.offer(left.left);
+            q.offer(right.right);
+            q.offer(left.right);
+            q.offer(right.left);
+
+        }
+        return true;
     }
 }
