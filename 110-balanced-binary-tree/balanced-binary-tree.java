104ed4594b81bdc853b3becffa306a83b4586265
VIVEK S <138084941+theviveks@users.noreply.github.com>
2025-10-15 10:21:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (73.67%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,32 +1,37 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public int findHeight(TreeNode root) {
-        if(root==null) return 0;
-        int lh = findHeight(root.left);
-        if (lh == -1)
-            return -1;
-        int rh = findHeight(root.right);
-        if (rh == -1)
-            return -1;
-        if(Math.abs(lh-rh)>1)return -1;
-        return Math.max(lh,rh)+1;
+    private int helper(TreeNode node, boolean[] ans) {
+        if (node == null) {
+            return 0;
+        }
+        int lh = helper(node.left, ans);
+        int rh = helper(node.right, ans);
+        if (Math.abs(lh - rh) > 1) {
+            ans[0] = false;
+        }
+        return 1 + Math.max(lh, rh);
     }
 
     public boolean isBalanced(TreeNode root) {
-        return findHeight(root) != -1;
+        if (root == null) {
+            return true;
+        }
+        boolean[] ans = { true };
+        helper(root, ans);
+        return ans[0];
     }
 }
