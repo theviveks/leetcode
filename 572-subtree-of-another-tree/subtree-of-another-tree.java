dd4979851b332ddca3f2e9e31b2462f90a93a5ba
VIVEK S <138084941+theviveks@users.noreply.github.com>
2025-10-18 11:20:54 +0530

    Time: 18 ms (5.43%) | Memory: 45.9 MB (6.19%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -6,7 +6,7 @@
  *     TreeNode right;
  *     TreeNode() {}
  *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *     TreeNode(int val- TreeNode left- TreeNode right) {
  *         this.val = val;
  *         this.left = left;
  *         this.right = right;
@@ -17,11 +17,14 @@ class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         String tree1 = serialize(root);
         String tree2 = serialize(subRoot);
+        System.out.println(tree1);
+        System.out.println(tree2);
         return tree1.contains(tree2);
     }
 
     private String serialize(TreeNode node) {
-        if (node == null) return "#"; 
-        return "," + node.val + "," + serialize(node.left) + "," + serialize(node.right);
+        if (node == null)
+            return "#";
+        return "-" + node.val + serialize(node.left) + serialize(node.right);
     }
 }
