0795865e2cc78f6868bccd28a70622948455d097
VIVEK S <138084941+theviveks@users.noreply.github.com>
2025-11-19 11:44:08 +0530

    Time: 7 ms (87.22%) | Memory: 46 MB (21.26%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int n = s.length();
+        int left = 0, right = 0;
+        int maxFreq = 0, maxLen = 0;
+        int[] hash = new int[26];
+        while (right < n) {
+            hash[s.charAt(right) - 'A']++;
+            maxFreq = Math.max(maxFreq, hash[s.charAt(right) - 'A']);
+            while ((right - left + 1) - maxFreq > k) {
+                hash[s.charAt(left) - 'A']--;
+                left++;
+            }
+            maxLen = Math.max(right - left + 1, maxLen);
+            right++;
+        }
+        return maxLen;
+    }
+}
