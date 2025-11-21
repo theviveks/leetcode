789ffdd6bb05064deb559c127e91917cedd93c52
VIVEK S <138084941+theviveks@users.noreply.github.com>
2025-11-21 21:49:55 +0530

    Time: 6 ms (85.29%) | Memory: 44.1 MB (41.85%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -18,15 +18,15 @@ class Solution {
         int[] hash1 = new int[26];
         int[] hash2 = new int[26];
         for (int i = 0; i < n; i++) {
-            hash1[s1.charAt(i) - 'a']++;
-            hash2[s2.charAt(i) - 'a']++;
+            hash1[s1.charAt(i) - 97]++;
+            hash2[s2.charAt(i) - 97]++;
         }
         for (int i = 0; i < m - n; i++) {
             if (hashMatches(hash1, hash2)) {
                 return true;
             }
             hash2[s2.charAt(i) - 97]--;
-            hash2[s2.charAt(i + n) - 'a']++;
+            hash2[s2.charAt(i + n) - 97]++;
 
         }
         return hashMatches(hash1, hash2);
