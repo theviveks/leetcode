44ed60876547468d9d442c97d999a71f16ab2a6b
VIVEK S <138084941+theviveks@users.noreply.github.com>
2025-12-12 11:46:05 +0530

    Time: 8 ms (88.27%) | Memory: 49.3 MB (12.59%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,13 +1,17 @@
 class Solution {
     public int[][] merge(int[][] nums) {
         int n = nums.length;
+        if (n == 0 || nums == null) {
+            return new int[0][0];
+        }
         List<int[]> ans = new ArrayList<>();
         Arrays.sort(nums, (a, b) -> Integer.compare(a[0], b[0]));
         ans.add(nums[0]);
         for (int i = 1; i < n; i++) {
             int[] last = ans.get(ans.size() - 1);
-            if (nums[i][0] <= last[1]) {
-                last[1] = Math.max(last[1], nums[i][1]);
+            int[] curr = nums[i];
+            if (curr[0] <= last[1]) {
+                last[1] = Math.max(last[1], curr[1]);
             } else {
                 ans.add(nums[i]);
             }
