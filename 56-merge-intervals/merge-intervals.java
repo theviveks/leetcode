095e57dc1e37f2bf62e9116158a4ed0dcfb027b4
VIVEK S <138084941+theviveks@users.noreply.github.com>
2025-12-12 11:37:21 +0530

    Time: 8 ms (88.27%) | Memory: 49.1 MB (21.97%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,7 +2,7 @@ class Solution {
     public int[][] merge(int[][] nums) {
         int n = nums.length;
         List<int[]> ans = new ArrayList<>();
-        Arrays.sort(nums, (a, b) -> a[0] - b[0]);
+        Arrays.sort(nums, (a, b) -> Integer.compare(a[0], b[0]));
         ans.add(nums[0]);
         for (int i = 1; i < n; i++) {
             int[] last = ans.get(ans.size() - 1);
