ad7672ff1b3adb01268e815e0b1fa6db6f979917
VIVEK S <138084941+theviveks@users.noreply.github.com>
2025-12-17 10:25:58 +0530

    Time: 4 ms (94.47%) | Memory: 43 MB (76.76%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -2,28 +2,17 @@ class Solution {
     public List<Integer> partitionLabels(String s) {
         int n = s.length();
         List<Integer> ans = new ArrayList<>();
-        int[] firstOccurance = new int[26];
         int[] lastOccurance = new int[26];
-        Arrays.fill(firstOccurance, -1);
         for (int i = 0; i < n; i++) {
-            int index = s.charAt(i) - 'a';
-            if (firstOccurance[index] == -1) {
-                firstOccurance[index] = i;
-            }
-            lastOccurance[index] = i;
+            lastOccurance[s.charAt(i) - 'a'] = i;
         }
-        int partitionStart = 0;
-        int partitionEnd = 0;
+        int partitionStart = 0, partitionEnd = 0;
         for (int i = 0; i < n; i++) {
-            if (partitionEnd < firstOccurance[s.charAt(i) - 'a']) {
-                ans.add(partitionEnd - partitionStart + 1);
-                partitionStart = i;
-                partitionEnd = i;
-            }
             partitionEnd = Math.max(partitionEnd, lastOccurance[s.charAt(i) - 'a']);
-        }
-        if (partitionEnd - partitionStart + 1 > 0) {
-            ans.add(partitionEnd - partitionStart + 1);
+            if (i == partitionEnd) {
+                ans.add(i - partitionStart + 1);
+                partitionStart = i + 1;
+            }
         }
         return ans;
     }
