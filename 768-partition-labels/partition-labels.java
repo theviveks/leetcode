eaf84ec026f3cc962c2106735eed869f5e881d07
VIVEK S <138084941+theviveks@users.noreply.github.com>
2025-12-17 10:14:04 +0530

    Time: 5 ms (81.96%) | Memory: 43.7 MB (35.79%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,13 +1,13 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        List<Integer> ans = new ArrayList<>();
         int n = s.length();
+        List<Integer> ans = new ArrayList<>();
         int[] firstOccurance = new int[26];
         int[] lastOccurance = new int[26];
         Arrays.fill(firstOccurance, -1);
         for (int i = 0; i < n; i++) {
             int index = s.charAt(i) - 'a';
-            if (firstOccurance[index] < 0) {
+            if (firstOccurance[index] == -1) {
                 firstOccurance[index] = i;
             }
             lastOccurance[index] = i;
@@ -15,13 +15,12 @@ class Solution {
         int partitionStart = 0;
         int partitionEnd = 0;
         for (int i = 0; i < n; i++) {
-            int curr = s.charAt(i) - 'a';
-            if (partitionEnd < firstOccurance[curr]) {
+            if (partitionEnd < firstOccurance[s.charAt(i) - 'a']) {
                 ans.add(partitionEnd - partitionStart + 1);
                 partitionStart = i;
                 partitionEnd = i;
             }
-            partitionEnd = Math.max(partitionEnd, lastOccurance[curr]);
+            partitionEnd = Math.max(partitionEnd, lastOccurance[s.charAt(i) - 'a']);
         }
         if (partitionEnd - partitionStart + 1 > 0) {
             ans.add(partitionEnd - partitionStart + 1);
